e13203edad1f94c81f07e11d2c3922faa09e767a
Kotlina <57289866+Zuraba69@users.noreply.github.com>
2021-07-06 14:24:28 +0400

    - added coordinator patterns

--- GalleryProject/Presentation/Scenes/Gallery/View/GalleryViewController.swift
+++ GalleryProject/Presentation/Scenes/Gallery/View/GalleryViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class GalleryViewController: UIViewController {
+class GalleryViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ GalleryProject/Presentation/Scenes/MainTabBar/View/MainTabBar.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBar.swift
+//  GalleryProject
+//
+//  Created by Zura Kobakhidze on 06.07.21.
+//
+
+import UIKit
+
+class MainTabBar: UITabBarController, Storyboarded, CoordinatorDelegate{
+    
+    var coordinator: AppCoordinatorProtocol?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
